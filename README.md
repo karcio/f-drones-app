3c2286a99bf92029d1f6cff8f6ad3c230bb12dfe
Karol Czarnecki <karcio@localhost.localdomain>
2019-04-02 21:35:05 +0100

    flask web application

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# f-drones-app
+# flight log drones web application
+
+## Description
+
+Drone flight logger - web application for FPV pilots:
+
+- track drones fleet
+- log flights with details
+
+## Requirements
+
+- python3, Flask
+- mysql installed
+
+## DB Configuration steps
+
+#### Execute SQL queries from static/assets/initdb.sql
+
+## Clone repository
+
+```
+git clone https://github.com/karcio/f-drones-app.git
+```
+
+## create virtual environment and activate
+
+```
+cd f-drones-app
+python3 -m venv virtenv
+source virtenv/bin/activate
+```
+
+## install requrements
+
+```
+pip install -r requirements.txt
+```
+
+## start application
+
+```
+python app.py
+```
---
+++ app.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template, session, request
+from flaskext.mysql import MySQL
+
+import os
+
+
+app = Flask(__name__)
+
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'sklejka'
+app.config['MYSQL_DATABASE_DB'] = 'RCDB'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+
+
+@app.route('/', methods=['GET'])
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+
+    else:
+        conn = mysql.connect()
+        cursor =conn.cursor()
+
+        cursor.execute("SELECT * from VIEW_TOTAL_FLIGHTS")
+        data = cursor.fetchone()  
+        print(data)  
+
+        return render_template('index.html', rows = data)
+
+
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'pa88w0rd' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+    else:
+        print('wrong password')
+    return home()
+
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return home()
+
+
+@app.route("/insert", methods=['GET', 'POST'])
+def insert():
+
+    if request.method == 'POST':
+        date = request.form['date']
+        place = request.form['place']
+        droneid = request.form['droneid']
+        lipo = request.form['lipo']
+        notes = request.form['notes']
+        
+        sql = "INSERT INTO FLIGHT_LOG(DATE, PLACE, DRONE_ID, LIPO, NOTES) VALUES('" + \
+            str(date)+"', '"+str(place)+"', "+str(droneid) + \
+            ", "+str(lipo)+", '"+str(notes)+"')"
+        print(sql)
+
+        conn = mysql.connect()
+        cursor =conn.cursor()
+        cursor.execute(sql)
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+        print('inserted 1 row: ' + sql)
+    return render_template('insert.html')
+    
+
+
+if __name__ == '__main__':
+    app.secret_key = os.urandom(12)
+    app.run(debug=True, host='0.0.0.0', port=5000)
