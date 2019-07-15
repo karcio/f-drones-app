92ebbb6c7add404ebe7fa543f639ec63bcec2b3f
Karol Czarnecki <karcio@localhost.localdomain>
2019-07-16 00:54:29 +0100

    set correct structure

---
+++ config.py
@@ -0,0 +1,67 @@
+import configparser
+import logging
+logging.basicConfig(
+    format=' %(levelname)s - %(asctime)s - %(message)s ', level=logging.DEBUG)
+
+config = configparser.ConfigParser()
+
+
+class configuration():
+
+    def getYear(self):
+        logging.info('Getting year')
+        self.readConfig()
+
+        return config.get('FOOTER', 'year')
+
+    def getCompany(self):
+        logging.info('Getting company')
+        self.readConfig()
+
+        return config.get('FOOTER', 'company')
+
+    def getLocalhost(self):
+        logging.info('Getting localhost')
+        self.readConfig()
+
+        return config.get('MY_SQL', 'localhost')
+
+    def getDatabase(self):
+        logging.info('Getting database')
+        self.readConfig()
+
+        return config.get('MY_SQL', 'database')
+
+    def getDbPass(self):
+        logging.info('Getting database password')
+        self.readConfig()
+
+        return config.get('MY_SQL', 'dbpassword')
+
+    def getDbUser(self):
+        logging.info('Getting database user')
+        self.readConfig()
+
+        return config.get('MY_SQL', 'dbuser')
+
+    def getAppVersion(self):
+        logging.info('Getting application version')
+        self.readConfig()
+
+        return config.get('APP', 'version')
+
+    def getAppPass(self):
+        logging.info('Getting application password')
+        self.readConfig()
+
+        return config.get('APP', 'password')
+
+    def getAppUser(self):
+        logging.info('Getting application user')
+        self.readConfig()
+        return config.get('APP', 'user')
+
+    def readConfig(self):
+        property = config.read('../f-drones-app/config')
+
+        return property
